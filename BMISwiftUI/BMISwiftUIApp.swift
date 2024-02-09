f35fb61561183e514860d7a765ace4533e72382a
JunichiHashimoto <gitonly543@gmail.com>
2024-02-09 17:59:34 +0900

    Initial Commit

---
+++ BMISwiftUI/BMISwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMISwiftUIApp.swift
+//  BMISwiftUI
+//
+//  Created by 橋本純一 on 2024/02/09.
+//
+
+import SwiftUI
+
+@main
+struct BMISwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
