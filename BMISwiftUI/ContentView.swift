8e4c83b41b42e4aa23cb59d4c9f144e6d83a7aa4
JunichiHashimoto <gitonly543@gmail.com>
2024-02-09 19:37:45 +0900

    feat: calc

--- BMISwiftUI/ContentView.swift
+++ BMISwiftUI/ContentView.swift
@@ -1,19 +1,49 @@
-//
-//  ContentView.swift
-//  BMISwiftUI
-//
-//  Created by 橋本純一 on 2024/02/09.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var weight: String = ""
+    @State var height: String = ""
+    @State var bmi: Double = 0
+    @State var classification: String = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("BMI Calculator").font(.largeTitle)
+            TextField("Enter weight (in kilograms)", text: $weight)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .border(Color.black)
+            TextField("Enter height (in kilograms)", text: $height)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .border(Color.black)
+            Button(action: {
+                if let weightDouble = Double(self.weight),
+                    let heightDouble = Double(self.height),
+                    heightDouble != 0 {
+                        self.bmi = weightDouble/(heightDouble * heightDouble)
+                        
+                        if self.bmi < 18.5 {
+                            self.classification = "Underweight"
+                        }
+                        else if bmi < 24.9 {
+                            self.classification = "Healthy Weight"
+                        }
+                        else if bmi < 29.9 {
+                            self.classification = "Overweight"
+                        }
+                        else {
+                            self.classification = "Obese"
+                        }
+                    }
+                    else {
+                        self.classification = "Invalid input"
+                    }
+            }) {
+                Text("Calculate BMI")
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(Color.blue)
+            }
         }
         .padding()
     }
